233912524e47b420c4996fca23dc4a1c926cb599
Muhammad Fauzan <muhammadfauzanfadhlulbarr@gmail.com>
2020-10-19 10:41:23 +0700

    first commit

---
+++ Latihan3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * Latihan3
+ */
+public class Latihan3 {
+
+    public static void main(String[] args) {
+        System.out.print("Masukan nama anda : ");
+        Scanner scanner = new Scanner(System.in);
+
+        String nama = scanner.next();
+        System.out.println("Nama anda adalah " + nama);
+
+    }
+}
